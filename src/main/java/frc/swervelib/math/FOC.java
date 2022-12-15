65a1325162d0f93952182ccb59f14063951043c7
Aidan747 <Aidan747-8@outlook.com>
2022-12-15 09:21:04 -0600

    fixed javadocs in FOC class

--- src/main/java/frc/swervelib/math/FOC.java
+++ src/main/java/frc/swervelib/math/FOC.java
@@ -2,7 +2,7 @@ package frc.swervelib.math;
 
 public class FOC {
 
-    /*
+    /**
     * @param currentAngle the angle of the robot as measured by the pidgeon from its starting position 
     * @param joyForward the joystick raw value for movement forwards and backwards
     * @param joyStrafe the joystick raw value from the strafing stick
